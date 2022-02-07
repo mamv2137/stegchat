aac1734e54b5235e86f3c0350a8e9c26c0d1ec3c
Mauricio Alejandro <malejandro@300dev.com>
2022-02-06 23:47:54 -0300

    add moralis and test it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { MoralisProvider } from 'react-moralis';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <MoralisProvider
+        appId="Yim1erWtKl7SbSYCgM8JvfZc6e0dDjmuU5rkYcFv"
+        serverUrl="https://nupuvx2w9fza.usemoralis.com:2053/server">
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
